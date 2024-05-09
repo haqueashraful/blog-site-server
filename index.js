614d6b96fcb8f4ac5a7be25eb49007d3c06a02a2
haqueashraful <123ashrafulhaque@gmail.com>
2024-05-10 01:33:17 +0600

    comment api

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
@@ -59,11 +59,92 @@ const verifyToken = (req, res, next) => {
     return res.status(401).json({ message: "Unauthorized" });
   }
 }
+const blogsCollection = client.db("myBlog").collection("blogs");
+const commentsCollection = client.db("myBlog").collection("comments");
 
 async function run() {
   try {
-    // token code
 
+    app.get("/blogs", async (req, res) => {
+      const cursor = blogsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.post("/blogs", async (req, res) => {
+      const blog = req.body;
+      const result = await blogsCollection.insertOne(blog);
+      res.send(result);
+    })
+
+
+    app.patch("/blogs/:id",logger, verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedBlog = req.body;
+      const result = await blogsCollection.updateOne(filter, {
+        $set: updatedBlog,
+      });
+      res.send(result);
+    })
+
+    app.delete("/blogs/:id", logger, verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await blogsCollection.deleteOne(query);
+      res.send(result);
+    })
+
+
+
+    // comment api
+app.post("/comments", logger, verifyToken, async (req, res) => {
+  try {
+    const { blogId, userName, email, userProfilePicture, commentText } = req.body;
+    const comment = {
+      blogId,
+      email,
+      userName,
+      userProfilePicture,
+      commentText,
+    };
+    const result = await commentsCollection.insertOne(comment);
+    res.send(result);
+  } catch (error) {
+    console.error("Error adding comment:", error);
+    res.status(500).send({ message: "Error adding comment" });
+  }
+});
+
+app.patch("/comments/:id", logger, verifyToken, async (req, res) => {
+  try {
+    const id = req.params.id;
+    const filter = { _id: new ObjectId(id) };
+    const updatedComment = req.body;
+    const result = await commentsCollection.updateOne(filter, {
+      $set: updatedComment,
+    });
+    res.send(result);
+  } catch (error) {
+    console.error("Error updating comment:", error);
+    res.status(500).send({ message: "Error updating comment" });
+  }
+})
+
+app.get("/blogs/:id/comments", async (req, res) => {
+  try {
+    const blogId = req.params.id;
+    const comments = await commentsCollection.find({ blogId }).toArray();
+    res.send(comments);
+  } catch (error) {
+    console.error("Error fetching comments:", error);
+    res.status(500).send({ message: "Error fetching comments" });
+  }
+});
+
+
+
+    // token code
     //creating Token
     app.post("/jwt", logger, verifyToken, async (req, res) => {
       const user = req.body;
